945c3d8f79534ce7937a2c9ffe1e53c12661df62
andrey-aka-skif <2119163@gmail.com>
2023-05-27 12:27:55 +0300

    Задача 4. Очевидно неверное решение, которое принято системой

---
+++ src/problem-004-median-of-two-sorted-arrays.tests/SolutionTest.cs
@@ -0,0 +1,36 @@
+namespace problem_004_median_of_two_sorted_arrays.tests;
+
+public class SolutionTest
+{
+    [Fact]
+    public void Leetcode_Case1()
+    {
+        // Arrange
+        var nums1 = new int[] { 1, 3 };
+        var nums2 = new int[] { 2 };
+        var expected = 2;
+
+        // Act
+        var solution = new Solution();
+        var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+        // Assert
+        Assert.Equal(result, expected);
+    }
+
+    [Fact]
+    public void Leetcode_Case2()
+    {
+        // Arrange
+        var nums1 = new int[] { 1, 2 };
+        var nums2 = new int[] { 3, 4 };
+        var expected = 2.5d;
+
+        // Act
+        var solution = new Solution();
+        var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+        // Assert
+        Assert.Equal(result, expected);
+    }
+}
---
+++ src/problem-004-median-of-two-sorted-arrays/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace problem_004_median_of_two_sorted_arrays;
+
+public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] newArray = new int[nums1.Length + nums2.Length];
+        Array.Copy(nums1, newArray, nums1.Length);
+        Array.Copy(nums2, 0, newArray, nums1.Length, nums2.Length);
+
+        Array.Sort(newArray);
+
+        bool isEven = newArray.Length % 2 == 0;
+
+        double median;
+
+        if (isEven)
+        {
+            int middleIndex1 = (newArray.Length / 2) - 1;
+            int middleIndex2 = middleIndex1 + 1;
+            median = (double)(newArray[middleIndex1] + newArray[middleIndex2]) / 2;
+        }
+        else
+        {
+            int middleIndex = newArray.Length / 2;
+            median = newArray[middleIndex];
+        }
+
+        return median;
+    }
+}
